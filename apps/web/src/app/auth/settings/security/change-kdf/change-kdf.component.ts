a9a8d2102e1685df5a920258e859ab1e32475708
vinith-kovan <vinith@kovanlabs.com>
2024-05-21 23:58:12 +0530

    migrating change-kdf component

--- apps/web/src/app/auth/settings/security/change-kdf/change-kdf.component.ts
+++ apps/web/src/app/auth/settings/security/change-kdf/change-kdf.component.ts
@@ -26,7 +26,6 @@ import { ChangeKdfConfirmationComponent } from "./change-kdf-confirmation.compon
 })
 export class ChangeKdfComponent implements OnInit {
   kdfConfig: KdfConfig = DEFAULT_KDF_CONFIG;
-  kdfType = KdfType;
   kdfOptions: any[] = [];
   private destroy$ = new Subject<void>();
 
@@ -69,71 +68,74 @@ export class ChangeKdfComponent implements OnInit {
 
   async ngOnInit() {
     this.kdfConfig = await this.kdfConfigService.getKdfConfig();
+    this.formGroup.get("kdf").setValue(this.kdfConfig.kdfType, { emitEvent: false });
     this.setFormControlValues(this.kdfConfig);
 
     this.formGroup
       .get("kdf")
       .valueChanges.pipe(takeUntil(this.destroy$))
       .subscribe((newValue) => {
-        let config;
-        const validators: {
-          iterations: ValidatorFn[];
-          memory: ValidatorFn[];
-          parallelism: ValidatorFn[];
-        } = {
-          iterations: [],
-          memory: [],
-          parallelism: [],
-        };
-
-        switch (newValue) {
-          case KdfType.PBKDF2_SHA256:
-            config = new PBKDF2KdfConfig();
-            validators.iterations = [
-              Validators.min(PBKDF2_ITERATIONS.min),
-              Validators.max(PBKDF2_ITERATIONS.max),
-            ];
-            break;
-          case KdfType.Argon2id:
-            config = new Argon2KdfConfig();
-            validators.iterations = [
-              Validators.min(ARGON2_ITERATIONS.min),
-              Validators.max(ARGON2_ITERATIONS.max),
-            ];
-            validators.memory = [
-              Validators.min(ARGON2_MEMORY.min),
-              Validators.max(ARGON2_MEMORY.max),
-            ];
-            validators.parallelism = [
-              Validators.min(ARGON2_PARALLELISM.min),
-              Validators.max(ARGON2_PARALLELISM.max),
-            ];
-            break;
-          default:
-            throw new Error("Unknown KDF type.");
-        }
-
-        this.kdfConfig = config;
-        this.formGroup.clearValidators();
-        this.setValidators("kdfConfig.iterations", validators.iterations);
-        this.setValidators("kdfConfig.memory", validators.memory);
-        this.setValidators("kdfConfig.parallelism", validators.parallelism);
-        this.setFormControlValues(this.kdfConfig);
+        this.updateKdfConfig(newValue);
       });
   }
-  setValidators(controlName: string, validators: ValidatorFn[]) {
+  private updateKdfConfig(newValue: KdfType) {
+    let config: KdfConfig;
+    const validators: { [key: string]: ValidatorFn[] } = {
+      iterations: [],
+      memory: [],
+      parallelism: [],
+    };
+
+    switch (newValue) {
+      case KdfType.PBKDF2_SHA256:
+        config = new PBKDF2KdfConfig();
+        validators.iterations = [
+          Validators.required,
+          Validators.min(PBKDF2_ITERATIONS.min),
+          Validators.max(PBKDF2_ITERATIONS.max),
+        ];
+        break;
+      case KdfType.Argon2id:
+        config = new Argon2KdfConfig();
+        validators.iterations = [
+          Validators.required,
+          Validators.min(ARGON2_ITERATIONS.min),
+          Validators.max(ARGON2_ITERATIONS.max),
+        ];
+        validators.memory = [
+          Validators.required,
+          Validators.min(ARGON2_MEMORY.min),
+          Validators.max(ARGON2_MEMORY.max),
+        ];
+        validators.parallelism = [
+          Validators.required,
+          Validators.min(ARGON2_PARALLELISM.min),
+          Validators.max(ARGON2_PARALLELISM.max),
+        ];
+        break;
+      default:
+        throw new Error("Unknown KDF type.");
+    }
+
+    this.kdfConfig = config;
+    this.setFormValidators(validators);
+    this.setFormControlValues(this.kdfConfig);
+  }
+
+  private setFormValidators(validators: { [key: string]: ValidatorFn[] }) {
+    this.setValidators("kdfConfig.iterations", validators.iterations);
+    this.setValidators("kdfConfig.memory", validators.memory);
+    this.setValidators("kdfConfig.parallelism", validators.parallelism);
+  }
+  private setValidators(controlName: string, validators: ValidatorFn[]) {
     const control = this.formGroup.get(controlName);
     if (control) {
       control.setValidators(validators);
       control.updateValueAndValidity();
     }
   }
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-  setFormControlValues(kdfConfig: KdfConfig) {
-    this.formGroup.get("kdf").setValue(kdfConfig.kdfType);
+  private setFormControlValues(kdfConfig: KdfConfig) {
+    this.formGroup.get("kdfConfig").reset();
     if (kdfConfig.kdfType === KdfType.PBKDF2_SHA256) {
       this.formGroup.get("kdfConfig.iterations").setValue(kdfConfig.iterations);
     } else if (kdfConfig.kdfType === KdfType.Argon2id) {
@@ -141,6 +143,11 @@ export class ChangeKdfComponent implements OnInit {
       this.formGroup.get("kdfConfig.memory").setValue(kdfConfig.memory);
       this.formGroup.get("kdfConfig.parallelism").setValue(kdfConfig.parallelism);
     }
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   isPBKDF2(t: KdfConfig): t is PBKDF2KdfConfig {
@@ -152,12 +159,16 @@ export class ChangeKdfComponent implements OnInit {
   }
 
   async openConfirmationModal() {
+    this.formGroup.markAllAsTouched();
+    if (this.formGroup.invalid) {
+      return;
+    }
     if (this.kdfConfig.kdfType === KdfType.PBKDF2_SHA256) {
-      this.kdfConfig.iterations = this.formGroup.value.kdfConfig.iterations;
+      this.kdfConfig.iterations = this.formGroup.get("kdfConfig.iterations").value;
     } else if (this.kdfConfig.kdfType === KdfType.Argon2id) {
-      this.kdfConfig.iterations = this.formGroup.value.kdfConfig.iterations;
-      this.kdfConfig.memory = this.formGroup.value.kdfConfig.memory;
-      this.kdfConfig.parallelism = this.formGroup.value.kdfConfig.parallelism;
+      this.kdfConfig.iterations = this.formGroup.get("kdfConfig.iterations").value;
+      this.kdfConfig.memory = this.formGroup.get("kdfConfig.memory").value;
+      this.kdfConfig.parallelism = this.formGroup.get("kdfConfig.parallelism").value;
     }
     this.dialogService.open(ChangeKdfConfirmationComponent, {
       data: {
