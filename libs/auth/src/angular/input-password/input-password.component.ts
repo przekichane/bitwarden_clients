677204f68db53705d00777123c0c62fc037170b5
rr-bw <102181210+rr-bw@users.noreply.github.com>
2024-05-28 16:17:14 -0700

    update validation

--- libs/auth/src/angular/input-password/input-password.component.ts
+++ libs/auth/src/angular/input-password/input-password.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { ReactiveFormsModule, FormBuilder, Validators } from "@angular/forms";
 
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
+import { Utils } from "@bitwarden/common/platform/misc/utils";
 import {
   AsyncActionsModule,
   ButtonModule,
@@ -10,7 +12,9 @@ import {
   InputModule,
 } from "@bitwarden/components";
 
+import { InputsFieldMatch } from "../../../../angular/src/auth/validators/inputs-field-match.validator";
 import { SharedModule } from "../../../../components/src/shared";
+import { PasswordCalloutComponent } from "../password-callout/password-callout.component";
 
 @Component({
   standalone: true,
@@ -25,23 +29,45 @@ import { SharedModule } from "../../../../components/src/shared";
     InputModule,
     ReactiveFormsModule,
     SharedModule,
+    PasswordCalloutComponent,
   ],
 })
 export class InputPasswordComponent {
-  minPasswordLength = 12;
+  minPasswordLength = Utils.minimumPasswordLength;
 
   currentHintLength = 0;
   minHintLength = 0;
   maxHintLength = 50;
 
-  passwordForm = this.formBuilder.group({
-    password: ["", Validators.required, Validators.minLength(this.minPasswordLength)],
-    confirmedPassword: ["", Validators.required],
-    hint: ["", Validators.maxLength(this.maxHintLength)],
-    checkForBreaches: [true],
-  });
+  passwordForm = this.formBuilder.group(
+    {
+      password: ["", Validators.required, Validators.minLength(this.minPasswordLength)],
+      confirmedPassword: ["", Validators.required, Validators.minLength(this.minPasswordLength)],
+      hint: [
+        null,
+        [
+          Validators.maxLength(this.maxHintLength),
+          InputsFieldMatch.validateInputsDoesntMatch(
+            "password",
+            this.i18nService.t("hintEqualsPassword"),
+          ),
+        ],
+      ],
+      checkForBreaches: [true],
+    },
+    {
+      validator: InputsFieldMatch.validateFormInputsMatch(
+        "password",
+        "confirmedPassword",
+        this.i18nService.t("masterPassDoesntMatch"),
+      ),
+    },
+  );
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private i18nService: I18nService,
+  ) {}
 
   submit() {}
 }
--- libs/auth/src/angular/input-password/input-password.stories.ts
+++ libs/auth/src/angular/input-password/input-password.stories.ts
@@ -24,17 +24,19 @@ export default {
             // Confirm master password
             characterMinimum: "__$1__ character minimum",
             confirmMasterPassword: "Confirm master password",
+            masterPassDoesntMatch: "Master password confirmation does not match",
 
             // Master password hint
             masterPassHintLabel: "Master password hint",
             masterPassHintText:
               "If you forget your password, the password hint can be sent to your email. __$1__/__$2__ character maximum.",
+            hintEqualsPassword: "Your hint cannot be your master password", // TODO-rr-bw: update this in messages.json to match figma
 
             // Other
             checkForBreaches: "Check known data breaches for this password",
             createAccount: "Create account",
 
-            // Validators
+            // Angular Validators
             required: "required",
             inputMaxLength: (max) => `51/${max} character maximum`,
 
