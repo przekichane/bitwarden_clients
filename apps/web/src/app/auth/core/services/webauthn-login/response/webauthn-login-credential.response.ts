76c4c6addeec8c0fe58afc8cb318f92c398d270f
Bernd Schoolmann <mail@quexten.com>
2024-06-05 15:55:54 +0200

    Remove encrypted private key from webauthnlogincredentialresponse

--- apps/web/src/app/auth/core/services/webauthn-login/response/webauthn-login-credential.response.ts
+++ apps/web/src/app/auth/core/services/webauthn-login/response/webauthn-login-credential.response.ts
@@ -9,7 +9,6 @@ export class WebauthnLoginCredentialResponse extends BaseResponse {
   id: string;
   name: string;
   prfStatus: WebauthnLoginCredentialPrfStatus;
-  encryptedPrivateKey?: string;
   encryptedPublicKey?: string;
   encryptedUserKey?: string;
 
@@ -18,7 +17,6 @@ export class WebauthnLoginCredentialResponse extends BaseResponse {
     this.id = this.getResponseProperty("Id");
     this.name = this.getResponseProperty("Name");
     this.prfStatus = this.getResponseProperty("PrfStatus");
-    this.encryptedPrivateKey = this.getResponseProperty("EncryptedPrivateKey");
     this.encryptedPublicKey = this.getResponseProperty("EncryptedPublicKey");
     this.encryptedUserKey = this.getResponseProperty("EncryptedUserKey");
   }
