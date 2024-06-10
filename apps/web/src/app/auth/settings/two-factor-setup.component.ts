fe12339f1c33eba38f7ea5a1d2178777ed29fe1b
vinith-kovan <vinith@kovanlabs.com>
2024-06-10 19:38:28 +0530

    two factor duo component migration

--- apps/web/src/app/auth/settings/two-factor-setup.component.ts
+++ apps/web/src/app/auth/settings/two-factor-setup.component.ts
@@ -163,17 +163,12 @@ export class TwoFactorSetupComponent implements OnInit, OnDestroy {
         if (!result) {
           return;
         }
-        const authComp: DialogRef<boolean, any> = TwoFactorDuoComponent.open(this.dialogService, {
+        const duoComp: DialogRef<boolean, any> = TwoFactorDuoComponent.open(this.dialogService, {
           data: result,
         });
-        authComp.componentInstance.onChangeStatus.subscribe((result: any) => {
-          this.updateStatus(result, TwoFactorProviderType.Duo);
+        duoComp.componentInstance.onChangeStatus.subscribe((enabled: boolean) => {
+          this.updateStatus(enabled, TwoFactorProviderType.Duo);
         });
-        // const duoComp = TwoFactorDuoComponent.open(this.dialogService, { data: result });
-        // const response: boolean = await lastValueFrom(duoComp.closed);
-        // if (response !== null) {
-        //   this.updateStatus(response, TwoFactorProviderType.Duo);
-        // }
         break;
       }
       case TwoFactorProviderType.Email: {
