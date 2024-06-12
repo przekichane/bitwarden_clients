f9b3ea0645c11c6c19212ef11036bd86243a3926
Addison Beck <hello@addisonbeck.com>
2024-06-12 12:06:46 -0500

    Document the `provider-permissions` guard in code

--- bitwarden_license/bit-web/src/app/admin-console/providers/guards/provider-permissions.guard.ts
+++ bitwarden_license/bit-web/src/app/admin-console/providers/guards/provider-permissions.guard.ts
@@ -6,6 +6,25 @@ import { Provider } from "@bitwarden/common/admin-console/models/domain/provider
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 
+/**
+ * `CanActivateFn` that asserts the logged in user has permission to access
+ * the page being navigated to. Two high-level checks are performed:
+ *
+ * 1. If the user is not a member of the provider in the URL parameters, they
+ *    are redirected to the home screen.
+ * 2. If the provider in  the URL parameters is disabled and the user is not
+ *    an admin, they are redirected to the home screen.
+ *
+ * In addition to these high level checks the guard accepts a callback
+ * function as an argument that will be called to check for more granular
+ * permissions. Based on the return from this callback one of the following
+ * will happen:
+ *
+ * 1. If the logged in user does not have the required permissions they are
+ *    redirected to `/providers`.
+ * 2. If the logged in user does have the required permissions navigation
+ *    proceeds as expected.
+ */
 @Injectable()
 export class ProviderPermissionsGuard implements CanActivate {
   constructor(
